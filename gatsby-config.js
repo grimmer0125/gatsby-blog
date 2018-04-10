410ed9442a04f0c2b98e4f28b1bcdbc8110e0929
Grimmer <grimmer0125@gmail.com>
2018-04-10 23:00:56 +0800

    add favicon.

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,6 +33,24 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: 'gatsby-plugin-favicon',
+      options: {
+        logo: './src/webIcon.png',
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          twitter: false,
+          yandex: false,
+          windows: false
+        }
+      }
+    },
+    {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/src/pages`,
