878c24e66ff319c1af794ccd15fbdfcb3f1da9e0
Grimmer <grimmer0125@gmail.com>
2018-04-12 23:24:42 +0800

    add some filter back for UI.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -55,8 +55,7 @@ export const pageQuery = graphql`
     }
     allMarkdownRemark(
         limit: 1000,
-        filter: { frontmatter: { layout: { eq: "post" } } },
-        # , draft: { ne: true }
+        filter: { frontmatter: { layout: { eq: "post" },  draft: { ne: true } } },
         sort: { order: DESC, fields: [frontmatter___date] }
       ){
       edges {
